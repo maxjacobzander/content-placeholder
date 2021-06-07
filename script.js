3e30be8016d71bcc0fe753c7b50dfb40ee28bb86
maxjacobzander <maxjacobzander@gmail.com>
2021-06-06 21:56:55 -0400

    Corrected getElements to getElement. Filled in data and removed animated class after setTimeout

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const header = document.getElementsById('header')
-const title = document.getElementsById('title')
-const excerpt = document.getElementsById('excerpt')
-const profile_img = document.getElementsById('profile_img')
-const name = document.getElementsById('name')
-const date = document.getElementsById('date')
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
 
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bgs_texts = document.querySelectorAll('.animated-bg-text')
 
+setTimeout(getData, 2500)
+
 function getData() {
     header.innerHTML = '<img src="https://maxjacobzander.netlify.app/static/Adrians_Dream-295827e8117ffe4b20385798165f35cd.jpg" alt="" />'
-    title.innerHTML = 'Lorem ipsum dolor sit amet.'
-    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, commodi!'
-    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
-    name.innerHTML = 'John Doe'
+    title.innerHTML = "Adrian's Dream"
+    excerpt.innerHTML = 'A website for a small crafting business'
+    profile_img.innerHTML = '<img src="https://static.wixstatic.com/media/b2c140_d6e7b1318122493b8faebfeb95af7c70~mv2_d_4480_6720_s_4_2.jpg/v1/fill/w_600,h_900,q_90/b2c140_d6e7b1318122493b8faebfeb95af7c70~mv2_d_4480_6720_s_4_2.jpg" alt="" />'
+    name.innerHTML = 'Max Zander'
     date.innerHTML = 'June 6, 2021'
+
+    animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+    animated_bgs_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
 }
